69b1c8334e369cfd688c2518f215c85d4783ebdf
komed3 <pk97@outlook.de>
2025-08-05 23:59:02 +0200

    add methods to scrap html pages

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 import csv
 import json
 import os
@@ -16,7 +16,10 @@ def ensure_db_folder ( path: str ) :
 
 def get_url ( config, key: str ) :
     if key in config.urls:
-        return config.urls[ key ]
+        url = config.urls[ key ]
+        for k in [ 'cpid', 'uid', 'username' ]:
+            url.replace( '$' + k, config[ k ] )
+        return url
     return None
 
 def open_stream ( path: str ) :
@@ -26,19 +29,22 @@ def open_stream ( path: str ) :
 def parse_table ( html: str, id: str ) :
     soup = BeautifulSoup( html, 'html.parser' )
     table = soup.find( 'table', { 'id': id } )
-    if not table:
-        return []
-    rows = table.find_all( 'tr' )[1:]
     data = []
-    for row in rows:
-        cols = [ td.get_text( strip=True ) for td in row.find_all( 'td' ) ]
-        if cols:
-            data.append( cols )
+    if table and isinstance( table, Tag ):
+        for row in table.find_all( 'tr' )[ 1: ]:
+            if not isinstance( row, Tag ):
+                continue
+            if cols := [ td.get_text( strip=True ) for td in row.find_all( 'td' ) ]:
+                data.append( cols )
     return data
 
 def main () :
     config = load_config( CFG )
     ensure_db_folder( DBM )
+
+    if url := get_url( config, 'lastDays' ):
+        if stream := open_stream( url ):
+            data = parse_table( stream, 'tblStats' )
 
 if __name__ == '__main__':
     main()
