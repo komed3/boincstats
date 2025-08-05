869e36a5cdf338e2bed24bf1c57b8cbba4e39df5
komed3 <pk97@outlook.de>
2025-08-05 23:20:16 +0200

    Create scraper.py

---
+++ src/scraper.py
@@ -0,0 +1,21 @@
+import os
+import json
+import csv
+from bs4 import BeautifulSoup
+
+CFG = '../config.json'
+DBM = '../db'
+
+def load_config ( path ) :
+    with open( path, 'r', encoding='utf-8' ) as f:
+        return json.load( f )
+
+def ensure_db_folder ( path ) :
+    os.makedirs( path, exist_ok=True )
+
+def main () :
+    config = load_config( CFG )
+    ensure_db_folder( DBM )
+
+if __name__ == '__main__':
+    main()
